c79617df275cf64e331c2a5399a85b7d120aeaf2
shaian <zshaian@users.noreply.github.com>
2025-02-07 15:06:07 +0800

    feat: improve audio playback on click event

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 alert("click anywhere to play the bad apple");
 
 const container = document.getElementById("container-element");
+const audio = new Audio("./assets/badapple.mp3");
 
-window.addEventListener("click", () => {
-  const audio = new Audio("./assets/badapple.mp3");
-
-  audio.loop = true;
-
+const start = () => {
+  console.log("only once");
   audio.play();
-
   setTimeout(() => {
     container.style.backgroundImage = "url('./assets/badapple.gif')";
   }, 500);
-});
+  window.removeEventListener("click", start);
+};
+
+audio.loop = true;
+
+window.addEventListener("click", start);
